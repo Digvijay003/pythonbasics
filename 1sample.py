09090a0114c5e7275ea660a3047a57bcf9e5a7ec
Digvijay003 <digvijay3singh@gmail.com>
2018-07-12 00:13:20 +0530

    Add files via upload

---
+++ 1sample.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+print "saaaasssssaaa"
+print 'hi'
+print("ho")
+print('he')
+a =56
+print(a)
+# this is for single commeent
+"""
+this is for multiple comment
+"""
+print (11%5)
+print "11%5"
+x = 100
+print x
+z = 10
+print "my my my  %d"%z
+print "ur age"
+age = raw_input()
+print "%r"%(age)
+from sys import argv
+script, first = argv # iska matlab argumanet variable ki unpacking aur uski values assign kar do  from left to right in order 
+print "script is ",script
+print "first is ", first
+
+q = "nkjdkjb"
+w = "jhffvkk"
+print q + w
+print"%r"%(q)
+
+
